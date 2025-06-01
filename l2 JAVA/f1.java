01de75374be501e7795a804175fc01255aca7f23
honesh singh <honeshsingh91@gmail.com>
2025-06-01 20:11:50 +0530

    Update f1.java and add f2.java

--- l2 JAVA/f1.java
+++ l2 JAVA/f1.java
@@ -1,5 +1,5 @@
 public class f1{
     public static void main(String []ard){
-        System.out.println("hell0");
+        System.out.println("kya dekh raha be");
     }
 }
---
+++ l2 JAVA/f2.java
@@ -0,0 +1,5 @@
+public class f2{
+    public static void main(String []ard){
+        System.out.println("kya dekh raha be anudip ke pap");
+    }
+}
